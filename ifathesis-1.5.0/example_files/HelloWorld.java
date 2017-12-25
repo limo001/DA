bbfe01a2571c9caf0f8eaa522bcc3f59b64d625e
moli001 <moli061066@gmail.com>
2017-12-25 13:58:47 +0100

    first up

---
+++ ifathesis-1.5.0/example_files/HelloWorld.java
@@ -0,0 +1,16 @@
+package de.tud.et.ifa;
+
+public class HelloWorld {
+	
+	/** 
+	 *	Das ist die Main-Methode in Java; sie gilt als 
+	 *	Einsprungpunkt für die Virtual Machine
+	 *  
+	 *	@param args Liste der Parameter, die beim Start des 
+	 *				Programms übergeben wurden
+	 *	@author Stefan Hennig, 2010
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
